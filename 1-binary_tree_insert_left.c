f221a3e196bfd801dc70368c3508d616cd10f95d
iyasu2 <iyasuforreal@gmail.com>
2023-08-31 19:24:30 +0300

    s

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,21 +10,25 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-	binary_tree_t *old_left = parent->left;
+	binary_tree_t *new_node;
 
-	if (new_node == NULL || parent == NULL)
+	if (parent == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = value;
 	new_node->parent = parent;
-	new_node->left = old_left;
+	new_node->left = parent->left;
 	new_node->right = NULL;
 
 	parent->left = new_node;
 
-	if (old_left != NULL)
-		old_left->parent = new_node;
+	if (new_node->left != NULL)
+		new_node->left->parent = new_node;
 
 	return (new_node);
 }
