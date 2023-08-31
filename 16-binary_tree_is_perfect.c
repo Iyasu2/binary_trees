be5cb2e42dadff8d8cc12419ca3be4a3fc1952c7
iyasu2 <iyasuforreal@gmail.com>
2023-08-31 19:10:39 +0300

    perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -42,3 +42,33 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	else
 		return (right_height + 1);
 }
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: the value to exponentiate
+ * @y: the power to raise x to
+ * Return: x to the power of y, or -1 if y is negative
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	else
+		return (x * _pow_recursion(x, y - 1));
+}
+
+/**
+* binary_tree_size - Measures the size of a binary tree.
+* @tree: Pointer to the root node of the tree to measure the size.
+*
+* Return: Size of the tree. If tree is NULL, return 0.
+*/
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
