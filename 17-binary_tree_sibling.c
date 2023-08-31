5219f1434ea7ca0fe94453a2eba2b3d1e931ff9b
iyasu2 <iyasuforreal@gmail.com>
2023-08-31 18:39:50 +0300

    s

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+
+/**
+ * binary_tree_sibling - entry point
+ *
+ * @node: parent node
+ * Return: binary_tree_t sibling node
+ */
+binary_tree_t binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+
+	if (node->parent->left == node)
+		return (node->parent->right);
+	else
+		return (node->parent->left);
+}
